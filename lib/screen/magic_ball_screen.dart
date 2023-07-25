ade8ee22effeb26cbd747e7c0fd8d8099c55a275
NaumovichID <naumova_i_d@sc.vsu.ru>
2023-07-25 14:16:07 +0300

    func: add basic interface

--- lib/screen/magic_ball_screen.dart
+++ lib/screen/magic_ball_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class MagicBallScreen extends StatelessWidget {
@@ -5,6 +7,47 @@ class MagicBallScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, Color.fromARGB(255, 211, 211, 255)],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 44.0,
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    log('Ball pressed!');
+                  },
+                  child: Image.asset(
+                    'assets/images/ball.png',
+                    height: 644,
+                    width: 644,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0, bottom: 80.0),
+                child: Text(
+                  'Press the magic ball or shake your phone',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color.fromARGB(255, 108, 105, 140),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
